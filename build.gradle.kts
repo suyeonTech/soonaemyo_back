404f474cc49664280e236a7b5dcc0154cb5d5cce
suyeonTech <wsy-ofc@naver.com>
2026-03-28 01:43:39 +0900

    fix: railway호환성을 위해 spring boot 버전 업그레이드

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     java
-    id("org.springframework.boot") version "3.3.0"
+    id("org.springframework.boot") version "3.4.4"
     id("io.spring.dependency-management") version "1.1.7"
     id("com.diffplug.spotless") version "8.1.0"
 }
